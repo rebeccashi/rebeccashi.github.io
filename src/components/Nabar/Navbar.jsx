9bf53bdb0efc75d68bc5b9b64417fc3b2f1fe7d6
Rebecca <rebecca_517@hotmail.com>
2020-07-03 20:44:34 -0400

    add logo links to navbar

--- src/components/Nabar/Navbar.jsx
+++ src/components/Nabar/Navbar.jsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { Link} from 'react-scroll';
 import { Col, Row } from 'react-bootstrap';
 import './NavbarStyles.css';
+import GithubLogo from '../../images/GithubLogo.png';
+import LinkedinLogo from '../../images/LinkedinLogo.png';
 
 function Navbar() {
     //Make an array for all the sections and save it in a separate file
     return (
             <div className='navbar'>
-                <h1 id='logo'>RS</h1>
+                <h1 id='initials'>RS</h1>
+                <Row className="logos">
+                    <Col><a href="https://github.com/rebeccashi"><img src={GithubLogo} alt='Github'/></a></Col>
+                    <Col><a href="https://github.com/rebeccashi"><img src={LinkedinLogo} alt='Linkedin'/></a></Col>
+                </Row>
                 <div className='links'>
                     <Row><Link activeClass="active" to='home' className='link' spy={true} smooth={true} duration={500}>Home</Link></Row>
                     <Row><Link activeClass="active" to='experience' className='link' spy={true} smooth={true} duration={500}>Experience</Link></Row>
