e78bf0696cf91354bb6375f7c5464050106c18e4
Rebecca <rebecca_517@hotmail.com>
2020-07-06 21:17:01 -0400

    navbar add logos

--- src/components/Nabar/Navbar.jsx
+++ src/components/Nabar/Navbar.jsx
@@ -2,17 +2,26 @@ import * as React from 'react';
 import { Link} from 'react-scroll';
 import { Col, Row } from 'react-bootstrap';
 import './NavbarStyles.css';
-import GithubLogo from '../../images/GithubLogo.png';
-import LinkedinLogo from '../../images/LinkedinLogo.png';
+import GithubLogo from '../../images/github.png';
+// import LinkedinLogo from '../../images/LinkedinLogo.png';
+import LinkedinLogo from '../../images/linkedin-logo.png';
 
 function Navbar() {
-    //Make an array for all the sections and save it in a separate file
+    //Make logos align in the center
     return (
             <div className='navbar'>
                 <h1 id='initials'>RS</h1>
                 <Row className="logos">
-                    <Col><a href="https://github.com/rebeccashi"><img src={GithubLogo} alt='Github'/></a></Col>
-                    <Col><a href="https://github.com/rebeccashi"><img src={LinkedinLogo} alt='Linkedin'/></a></Col>
+                    <Col>
+                        <a href="https://github.com/rebeccashi" target="_blank">
+                            <img src={GithubLogo} alt='Github'/>
+                        </a>
+                    </Col>
+                    <Col>
+                        <a href="https://www.linkedin.com/in/xiaoyi-s-b312a3106/" target="_blank">
+                            <img src={LinkedinLogo} alt='Linkedin' id='linkedin'/>
+                        </a>
+                    </Col>
                 </Row>
                 <div className='links'>
                     <Row><Link activeClass="active" to='home' className='link' spy={true} smooth={true} duration={500}>Home</Link></Row>
