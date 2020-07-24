b0fa944b35c552f8c219d6cbfb9cee9474f02df1
Rebecca <rebecca_517@hotmail.com>
2020-07-24 13:51:11 -0400

    makes boxes responsive

--- src/components/Experience/BoxWrapper.jsx
+++ src/components/Experience/BoxWrapper.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import './Box.css';
 import p5 from 'p5';
 import Box from './Box';
+import './BoxWrapper.css';
 
 class BoxWrapper extends React.Component {
     
     componentDidMount() {
         this.canvas = new p5(Box, "boxSketch");
-        console.log(this.props);
         //the name in quotation marks corresponds to the div id
-    }
-
-    shouldComponentUpdate() { // just in case
-        return false;
     }
 
     componentWillUnmount() {
@@ -20,17 +16,39 @@ class BoxWrapper extends React.Component {
     }
 
     render() {
-        const {type, summary, details} = this.props.object;        
+        const {type, summary, details, index} = this.props.object;        
+        let parentWidth, parentHeight, width, height, marginLeft;
+        if (this.props.width > 0) {
+            parentWidth = this.props.width;
+            console.log('Box props' + parentWidth);
+            if (parentWidth > 800) {    //3 boxes
+                width = parentWidth/3;
+                if (index % 3 === 1) {
+                    marginLeft = width - 150; 
+                } else if (index % 3 === 2) {
+                    marginLeft = 0;
+                }
+            } else if (parentWidth > 500) { //2 boxes
+                width = parentWidth/2;
+            } else {
+                width = parentWidth;
+            }
 
-        console.log(this.props);
+        }
+        if (this.props.height > 0) {
+            parentHeight = this.props.height;
+            console.log('Box props' + parentHeight);
+        }
+
+        //fix summary style
 
         return(
-            <div id="boxSketch">
+            <div id="boxSketch" style={{width: width, marginLeft: marginLeft}}>
                 <div id='type'>{type}</div>
                      <h3 className='summary'>
-                         <span>{summary.name}, </span>
-                         <span>{summary.company}, </span>
-                         <span>{summary.location}</span>
+                         <span id="name">{summary.name}</span>
+                        { summary.company && <span id="company">, {summary.company}, </span>}
+                        { summary.location && <span>{summary.location}</span>}
                      </h3>
                      <ul>
                          {details.map(detail => <li>{detail}</li>)}
