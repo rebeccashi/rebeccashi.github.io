ad8701f85b173ffb532e1445060615a640f47ace
Rebecca <rebecca_517@hotmail.com>
2020-07-24 13:57:16 -0400

    one paragraph home page

--- src/components/Home/HomeWrapper.js
+++ src/components/Home/HomeWrapper.js
@@ -28,20 +28,29 @@ class HomeWrapper extends React.Component {
                     <Col md={1}></Col>
                     <Col md={10} className='bio'>
                         <h2>Hi there!</h2>
-                        <p className='first'>
+                        <p>
+                            I am Xiaoyi Shi and I go by Rebecca. 
+                            I am a rising junior at <span id='nyu'>New York University</span> studying <span id='cs'>Computer Science</span>. 
+                            So far, I have worked as a front-end developer
+                            using <span id='skills'>React.js, Typescript, and Node.js.</span>
+                            &nbsp;My goal is to become a full stack software engineer. 
+                            In addition to computer science, 
+                            I am also interested in <span id='art'>art and design,</span> including UI/UX.
+                        </p>
+                        {/* <p className='first'>
                             I am Xiaoyi Shi and I go by Rebecca. <br />
                             I am a rising junior at <span id='nyu'>New York University</span> studying <span id='cs'>Computer Science</span>. 
                         </p>
                         <p className='second'>
                             So far, I have worked as a front-end developer<br/>
                             using <span id='skills'>React.js, Typescript, and Node.js.</span> <br/>
-                            {/* maybe highlight this part instead */}
+                            maybe highlight this part instead
                             My goal is to become a full stack software engineer. 
                         </p>
                         <p className='third'>
                             In addition to computer science, 
                             I am also interested in <span id='art'>art and design,</span> including UI/UX. 
-                        </p>
+                        </p> */}
                     </Col>
                     <Col md={1}></Col>
                 </Row>
