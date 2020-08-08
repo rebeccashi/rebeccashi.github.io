697ed009cafb2da5c0a9aa41307acd41b463a48d
Rebecca <rebecca_517@hotmail.com>
2020-08-07 20:11:23 -0400

    add home background design

--- src/components/Home/HomeBackground.js
+++ src/components/Home/HomeBackground.js
@@ -17,10 +17,26 @@ const HomeBackground = (p) => {
         p.background(255);
         canvas.position(0,0);
         canvas.style('z-index', -1);
+        p.rectMode(p.CENTER);
+        p.angleMode(p.DEGREES);
     }
 
     p.draw = () => {
-        
+        let textWidth = width * 10 / 12;
+        // console.log(`window height: ${p.windowHeight}`)
+        let textLeft = width/12;
+        //marginTop is 10%
+        let textTop = p.windowHeight * 0.18;
+        let centerLeft = textLeft + textWidth/2;
+        let centerTop = textTop + 350/2;
+        //TODO: find a way to calculate textHeight
+        p.noFill();
+        p.strokeWeight(1)
+        p.rect(centerLeft, centerTop, textWidth, 350)
+        p.rotate(-10);
+        p.translate(-90, 50)
+        p.rect(centerLeft, centerTop, textWidth, 370)
+        //do math to calculate rotated rect's width & height with regards to angle
     }
 };
 
