25de8cfe0b980c5f48630fc42f0e82922d053aad
Rebecca <rebecca_517@hotmail.com>
2020-08-09 12:57:46 -0400

    preliminary rect design

--- src/components/Home/HomeBackground.js
+++ src/components/Home/HomeBackground.js
@@ -17,26 +17,35 @@ const HomeBackground = (p) => {
         p.background(255);
         canvas.position(0,0);
         canvas.style('z-index', -1);
-        p.rectMode(p.CENTER);
+        // p.rectMode(p.CENTER);
         p.angleMode(p.DEGREES);
     }
 
     p.draw = () => {
         let textWidth = width * 10 / 12;
-        // console.log(`window height: ${p.windowHeight}`)
         let textLeft = width/12;
         //marginTop is 10%
-        let textTop = p.windowHeight * 0.18;
+        let textTop = p.windowHeight * 0.2;
         let centerLeft = textLeft + textWidth/2;
         let centerTop = textTop + 350/2;
         //TODO: find a way to calculate textHeight
         p.noFill();
         p.strokeWeight(1)
-        p.rect(centerLeft, centerTop, textWidth, 350)
-        p.rotate(-10);
+        p.rect(textLeft, textTop, textWidth, 350)
+        // p.rect(centerLeft, centerTop, textWidth, 350)
+        p.rotate(-11);
         p.translate(-90, 50)
-        p.rect(centerLeft, centerTop, textWidth, 370)
-        //do math to calculate rotated rect's width & height with regards to angle
+        p.rect(textLeft, textTop, textWidth, 370)
+        // p.rect(centerLeft, centerTop, textWidth, 370)
+
+        // p.rotate(-22);
+        // p.translate(-110,30)
+        // p.rect(textLeft, textTop, textWidth * 0.8, 400)
+
+        p.rotate(20);
+        p.translate(87, -140)
+        p.rect(textLeft, textTop, textWidth, 360)
+
     }
 };
 
