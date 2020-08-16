0e68f38c539a95987376056caad764816b173f66
Rebecca <rebecca_517@hotmail.com>
2020-08-16 19:25:00 -0400

    Coursework Section

---
+++ src/components/Coursework/Constants.js
@@ -0,0 +1,9 @@
+const courses = [
+    'Data Structures',
+    'Basic Algorithms',
+    'Computer Security',
+    'Computer Systems Organization',
+    'Linear Algebra', 
+]
+
+export {courses};
--- src/components/Coursework/Coursework.jsx
+++ src/components/Coursework/Coursework.jsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import {Row, Col} from 'react-bootstrap';
+import { courses } from './Constants';
+import './Coursework.css'
 
 const Coursework = () =>{
     return (
-        <div>
-            Coursework
+        <div id='coursework'>
+        {/*Top school work section */}
+            <Row>
+                <Col sm={6}>
+                    <p className='courseHeading'>Courses</p>
+                    <ul>
+                        {courses.map(
+                            (course, index) => <li key={index}>{course}</li>
+                        )}
+                    </ul>
+                </Col>
+                <Col sm={6}></Col>
+            </Row>
+            <Row>
+                <Col sm={3}></Col>
+            </Row>
         </div>
     )
 };
