d803c26fb8c03deaa20fde85cc585f478b95c1d3
Rebecca <rebecca_517@hotmail.com>
2020-09-04 13:39:57 -0400

    Updates

--- src/colors.js
+++
@@ -1,7 +0,0 @@
-const darkPurple='#6E44FF';
-const sand = '#EDC8AD';
-const green = '#84E6F8';
-const lightPurple = '#BAB5FF';
-
-
-export const {darkPurple, lightPurple, sand, green} = themeColors;
