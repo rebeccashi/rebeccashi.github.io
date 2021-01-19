ca64524be0f202bffce33cf4a8a59b925f0cf378
Rebecca <rebecca_517@hotmail.com>
2021-01-19 11:46:05 -0500

    topbar styling

--- src/Mobile/TopBar/TopBar.jsx
+++ src/Mobile/TopBar/TopBar.jsx
@@ -27,11 +27,11 @@ class TopBar extends React.Component{
         return (
             <div id='topbar'>
                 <Row>
-                    <Col xs={9} className='initials'>
+                    <Col xs={8} className='initials'>
                         <h1>RS</h1>
                     </Col>
-                    <Col xs={3} className='menuIcon'>
-                        <img src={MenuIcon}/>
+                    <Col xs={4} className='menuIcon'>
+                        <img src={MenuIcon} id='icon'/>
                     </Col>
                 </Row>
             </div>
