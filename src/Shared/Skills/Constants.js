f7e345fb9f8165550d03403e8236387b472edd09
Rebecca <rebecca_517@hotmail.com>
2021-01-26 13:54:12 -0500

    skills constants

---
+++ src/Shared/Skills/Constants.js
@@ -0,0 +1,26 @@
+const frontend = [
+    {skill: "Javascript", value: 4},	//0
+    {skill: "React", value: 5},			//1
+    {skill: "Typescript", value: 3},	//2
+    {skill: "HTML", value: 4},		    //3
+    {skill: "CSS", value: 4},			//4
+    {skill: "Node.js",value: 3},	    //5
+];
+
+const others = [
+    {skill: "Java", value: 5},      //0
+    {skill: "C", value: 3},         //1
+    {skill: "Python", value: 4},    //2
+    {skill: "git", value: 4},       //3
+    {skill: "OpenSSL", value: 3},   //4
+    {skill: "Swift", value: 3}      //5
+];
+
+const art = [
+    {skill: "Photoshop", value: 3},     //0
+    {skill: "Web Design", value: 4},    //1
+    {skill: "P5.js", value: 3},         //2
+    {skill: "Digital Art", value: 5},   //3
+]
+
+export { frontend, others, art };
