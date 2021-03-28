3532cfd49b840de47bf503c3c74273e5bb65cc81
Rebecca <rebecca_517@hotmail.com>
2021-03-28 17:10:43 -0400

    Updates

---
+++ precache-manifest.b83a7f92e8ced7eaf6ca04b17768bea9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed3de40433a91baab438014462c46d6",
+    "url": "/rebeccashi.github.io/index.html"
+  },
+  {
+    "revision": "d21fe51d89cd77caddf1",
+    "url": "/rebeccashi.github.io/static/css/main.82e66a63.chunk.css"
+  },
+  {
+    "revision": "656fa5d638ce10f3a799",
+    "url": "/rebeccashi.github.io/static/js/2.58b39c87.chunk.js"
+  },
+  {
+    "revision": "1991551b7858c0979a0db5af1a0a56af",
+    "url": "/rebeccashi.github.io/static/js/2.58b39c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21fe51d89cd77caddf1",
+    "url": "/rebeccashi.github.io/static/js/main.a876ed9d.chunk.js"
+  },
+  {
+    "revision": "2c59f67cb281fc21473a",
+    "url": "/rebeccashi.github.io/static/js/runtime-main.b6d03aa8.js"
+  },
+  {
+    "revision": "dcef4d9450396faf1b8a813f12b7b283",
+    "url": "/rebeccashi.github.io/static/media/calendar-snapshot.dcef4d94.png"
+  },
+  {
+    "revision": "f34e408095318d2d2da2954979b4f20d",
+    "url": "/rebeccashi.github.io/static/media/cssa.f34e4080.png"
+  },
+  {
+    "revision": "81386d95108dc97085d7e3ad993dc866",
+    "url": "/rebeccashi.github.io/static/media/cumulus.81386d95.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rebeccashi.github.io/precache-manifest.773bc0cda6f60336f96509c4c1c346c7.js"
+  "/rebeccashi.github.io/precache-manifest.b83a7f92e8ced7eaf6ca04b17768bea9.js"
 );
 
 self.addEventListener('message', (event) => {
