57e80eb217cce0d721ac09098c134c9d9735916c
Rebecca <rebecca_517@hotmail.com>
2021-04-03 21:36:00 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rebeccashi.github.io/precache-manifest.a437e4a8b6430ae8fecb7f7fbcf44e92.js"
+  "/rebeccashi.github.io/precache-manifest.26417b5ebd44d218c3fffab10d434132.js"
 );
 
 self.addEventListener('message', (event) => {
